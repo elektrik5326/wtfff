5b3746c8aa740a49aea40289557b2cddb3b45a14
He_IIIk0lHIIk <elektrik5326@gmail.com>
2026-03-26 16:22:47 +0300

    v1.1

--- ffa.py
+++ ffa.py
@@ -2,6 +2,10 @@ import sqlite3
 from flask import Flask,request,render_template
 
 is_admin = False
+is_developer = False
+result = ""
+result_develop = ""
+
 app = Flask(__name__)
 def init_db():
     conn = sqlite3.connect('data.db')
@@ -19,27 +23,49 @@ def add_data(word,comm,name):
 
 @app.route('/login', methods=['POST', 'GET'])
 def main():
+    global result_develop
+    global result
     global is_admin
+    global is_developer
+
+
     if request.method == 'POST':
         name = request.form.get('password')
-        if name == '1234':
-            is_admin = True
-    return render_template('login.html')
+        pas = request.form.get('pass')
+        button = request.form.get('sumbit')
+        if button == 'admin':
+            if name == '1234':
+                is_admin = True
+                result = 'Вы успешно вошли, теперь вам доступна админ-панель'
+                result_develop = ""
+            else:
+                result = 'Вы не вошли, пароль: 1234'
+                result_develop = ""
+        elif button == 'developer':
+            if pas == '2014':
+                is_developer = True
+                result_develop = 'Вы успешно вошли'
+                result = ""
+            else:
+                result = ""
+                result_develop = 'Вы не вошли'
+
+    return render_template('login.html', result=result, result_develop=result_develop)
 
 @app.route('/', methods=['POST', 'GET'])
 def users():
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute('SELECT * FROM main')
+    c.execute('SELECT *, rowid FROM main')
     data = c.fetchall()
 
-    return render_template('index.html', main=data)
+    return render_template('index.html', main=data, is_developer=is_developer)
 
 @app.route('/adminpanel', methods=['POST', 'GET'])
 def admin_panel():
     global is_admin
     if is_admin == False:
-        return '<h1>Вы не вошли</h1><br><a href="/">Обратно</a>'
+        return render_template('notadmin.html')
     elif is_admin == True:
         if request.method == 'POST':
             word = int(request.form.get('word'))
@@ -63,9 +89,33 @@ def admin_panel():
                 return render_template('admin.html')
         elif request.method == 'GET':
             return render_template('admin.html')
-init_db()
+        
+@app.route('/developerpanel', methods=['POST','GET'])
+def developer():
+    if is_developer == False:
+        return render_template('notadmin.html')
+    else:
+        btn = request.form.get('btn')
+        text = request.form.get('rowid')
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+        if btn == "value":
+            conn = sqlite3.connect('data.db')
+            c = conn.cursor()
+            c.execute('DELETE FROM main')
+            conn.commit()
+            conn.close()
+            result_all = 'Все очищено'
+            return render_template('developer.html', result_all=result_all)
+        elif btn == 'value1':
+            conn = sqlite3.connect('data.db')
+            c = conn.cursor()
+            c.execute(f'DELETE FROM main WHERE rowid = {text}')
+            conn.commit()
+            conn.close()
+            
+            result_rowid = f'Успешно удалено сообщение по ID = {text}'
+            return render_template('developer.html',result_rowid=result_rowid)
+        return render_template('developer.html')
+    
+init_db()
+app.run()
